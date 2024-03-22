2471b8aac74b84ef6ebc22405dedd3d59b62c7e1
Abhishek kumar mishra <118496810+MishraAbhishek1@users.noreply.github.com>
2024-03-22 14:31:05 +0530

    Update Sorting_an_ArrayList.java

--- Sorting_an_ArrayList.java
+++ Sorting_an_ArrayList.java
@@ -4,7 +4,7 @@ public class Sorting_an_ArrayList {
 
 	public static void main(String[] args) {
 		ArrayList<Integer>list=new ArrayList();
-		list.add(2);
+		list.add(7);
 		list.add(5);
 		list.add(9);
 		list.add(6);
